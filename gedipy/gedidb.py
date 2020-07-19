96b9acfae401558f1aae7198c188eca90314e779
John Armston <armston@umd.edu>
2020-07-19 15:52:17 -0400

    added frame function to GEDIShotNumber class, and create ShotNumber parent class

--- gedipy/gedidb.py
+++ gedipy/gedidb.py
@@ -18,6 +18,29 @@ GEDIPY_FINDER_URL = 'https://lpdaacsvc.cr.usgs.gov/services/gedifinder'
 
 
 class ShotNumber:
+    """Parent class for shot numbers"""
+    def __init__(self, shot_number):
+        self.shot_number = shot_number
+
+
+class GEDIShotNumber(ShotNumber):
+    """
+    Generic object for GEDI shot numbers
+
+    Format: OOOOOBBFFFNNNNNNNN
+
+	where
+	OOOOO: Orbit number
+	BB: Beam number
+	FFF: Minor frame number (0-241)
+	NNNNNNNN: Shot number within orbit
+
+    If a packet is dropped (never received on the ground), NNNNNNNN will not
+    save space for it; however, if a packet with a bad CRC is received it will
+    be ‘skipped’ in NNNNNN. The bad CRC packet will not be in the L1A data, but
+    its place is held in case it can be corrected in processing updates.
+    """
+
     def __init__(self, shot_number):
         self.shot_number = shot_number
 
@@ -26,6 +49,9 @@ class ShotNumber:
 
     def get_beam(self):
         return (self.shot_number % 10000000000000) // 100000000000
+
+    def get_frame(self):
+        return (self.shot_number % 100000000000) // 100000000
 
     def get_index(self):
         return self.shot_number % 100000000
@@ -37,12 +63,12 @@ class ShotNumber:
 class FileDatabase:
     def __init__(self):
         self.file_by_orbit = {}
-    
+
     def get_reader(self, filename):
         for cls in h5io.LidarFile.__subclasses__():
             try:
                 h5_file = cls(filename)
-                h5_file.open()
+                h5_file.open_h5()
                 return h5_file
             except h5io.GEDIPyDriverError:
                 pass
@@ -85,7 +111,7 @@ class FileDatabase:
 
     def query_gedi_finder(self, bbox, product='GEDI02_B', version=1, output='json', localroot=None):
         url_template = '{}?product={}&version={:03d}&bbox=[{:f},{:f},{:f},{:f}]&output={}'
-        url = url_template.format(GEDIPY_FINDER_URL, product, version, 
+        url = url_template.format(GEDIPY_FINDER_URL, product, version,
                                   bbox[1], bbox[0], bbox[3], bbox[2], output)
         handle = urllib.request.urlopen(url)
         json_str = handle.read()
@@ -112,7 +138,7 @@ class ShotNumberDatabase:
         with open(filename, 'r') as fid:
             for line in fid:
                 if len(line) > 1:
-                    sn = ShotNumber(int(line.strip()))
+                    sn = GEDIShotNumber(int(line.strip()))
                     orbit = sn.get_orbit_number()
                     beam = sn.get_beam()
                     if orbit not in self.shot_numbers_by_orbit_beam:
@@ -121,7 +147,7 @@ class ShotNumberDatabase:
                         self.shot_numbers_by_orbit_beam[orbit][beam] = []
                     self.shot_numbers_by_orbit_beam[orbit][beam].append(sn)
 
-    def add_shot_numbers_from_h5(self, filename):
+    def add_gedi_shot_numbers_from_h5(self, filename):
         f = h5py.File(filename, 'r')
         for group in f.keys():
             if group.startswith('BEAM'):
@@ -129,10 +155,9 @@ class ShotNumberDatabase:
                 orbit = shot_number // 10000000000000
                 beam = (shot_number % 10000000000000) // 100000000000
                 for i in range(shot_number.size):
-                    sn = ShotNumber(shot_number[i])
+                    sn = GEDIShotNumber(shot_number[i])
                     if orbit[i] not in self.shot_numbers_by_orbit_beam:
                         self.shot_numbers_by_orbit_beam[orbit[i]] = {}
                     if beam[i] not in self.shot_numbers_by_orbit_beam[orbit[i]]:
                         self.shot_numbers_by_orbit_beam[orbit[i]][beam[i]] = []
                     self.shot_numbers_by_orbit_beam[orbit[i]][beam[i]].append(sn)
-
